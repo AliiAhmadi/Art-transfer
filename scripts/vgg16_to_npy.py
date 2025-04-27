6078e88b3b5756520164a5b969cd9787468de258
Ali Ahmadi <aliahmadi.github@gmail.com>
2025-04-27 16:51:56 +0330

    converters (from Tensorflow format to npy)

--- scripts/vgg16_to_npy.py
+++ scripts/vgg16_to_npy.py
@@ -1,1 +1,20 @@
 #! /bin/python3
+
+import tensorflow as tf
+import numpy as np
+
+vgg16 = tf.keras.applications.VGG16(include_top=True, weights='imagenet')
+
+vgg16_npy = {}
+
+for layer in vgg16.layers:
+    if isinstance(layer, tf.keras.layers.Conv2D) or isinstance(layer, tf.keras.layers.Dense):
+        weights = layer.get_weights()
+        if len(weights) == 2:
+            W, b = weights
+            vgg16_npy[layer.name] = [W, b]
+        else:
+            print(f"Skipping layer {layer.name} — unexpected weights format.")
+
+np.save('../models/vgg16.npy', vgg16_npy)
+print("Saved vgg16.npy!")
--- scripts/vgg19_to_npy.py
+++ scripts/vgg19_to_npy.py
@@ -1,1 +1,20 @@
 #! /bin/python3
+
+import tensorflow as tf
+import numpy as np
+
+vgg19 = tf.keras.applications.VGG19(include_top=True, weights='imagenet')
+
+vgg19_npy = {}
+
+for layer in vgg19.layers:
+    if isinstance(layer, tf.keras.layers.Conv2D) or isinstance(layer, tf.keras.layers.Dense):
+        weights = layer.get_weights()
+        if len(weights) == 2:
+            W, b = weights
+            vgg19_npy[layer.name] = [W, b]
+        else:
+            print(f"Skipping layer {layer.name} — unexpected weights format.")
+
+np.save('../models/vgg19.npy', vgg19_npy)
+print("Saved vgg19.npy!")
