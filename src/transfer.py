16c6bf3106981ca52108ff6cb2cc5c1527470f02
Ali Ahmadi <aliahmadi.github@gmail.com>
2025-04-27 22:44:37 +0330

    -

--- src/transfer.py
+++ src/transfer.py
@@ -6,3 +6,6 @@ import torch.optim as optim
 from torchvision import transforms, models
 import cv2
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = models.vgg16(pretrained = True)
